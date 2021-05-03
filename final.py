9c4518d84b28c09a04b53a1ee57b28a26992e6f7
Melody <diao1010@gmail.com>
2021-05-03 03:11:21 -0500

    Add files via upload

--- final.py
+++ final.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import numpy as np
 
 
 def get_max_lap(df: pd.DataFrame) -> pd.DataFrame:
@@ -13,7 +12,7 @@ def get_max_lap(df: pd.DataFrame) -> pd.DataFrame:
     return df[(df['positionOrder'] == 1) & (df['stop'] == 1)].reset_index(drop=True)[['raceId', 'year', 'laps']]
 
 
-def count_proportion(df_pit: pd.DataFrame, df_race: pd.DataFrame) -> pd.DataFrame:
+def count_proportion(df_pit: pd.DataFrame, df_race: pd.DataFrame, decimal: int) -> pd.DataFrame:
     """
     add column with the proportion during the total laps in each race
     :param df_pit: pit dataframe
@@ -21,8 +20,66 @@ def count_proportion(df_pit: pd.DataFrame, df_race: pd.DataFrame) -> pd.DataFram
     :return:
     """
     df_new = df_pit.merge(df_race[['raceId', 'laps']], on='raceId', how='left', suffixes=('_once', '_total'))
-    df_new['proportion'] = round(df_new['lap'] / df_new['laps'], 1)
+    df_new['proportion'] = round(df_new['lap'] / df_new['laps'], decimal)
     return df_new
+
+
+def select_finished_data(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    drop data rows with unfinished status
+    :param df: input dataframe
+    :return: modified dataframe
+    """
+    status_in = [1, 11, 12, 13, 14, 15, 16, 17, 18, 19]
+    return df.drop(df[~df['statusId'].isin(status_in)].index, inplace=True)
+
+
+def plot_line_chart(df: pd.DataFrame, stop: int, plot_title):
+    """
+    This function the x and y co-ordinates to plot a line graph
+    :param df: input dataframe ready for plotting
+    :param stop: pit stop times
+    :param plot_title: title for the line graph
+    :return:
+    """
+    fig, ax = plt.subplots(figsize=(10, 5))
+    if stop == 1:
+        df.plot(x='proportion', y='laps', ax=ax)
+    elif stop >= 2:
+        # first stop
+        x1 = df[df['stop'] == 1]['proportion']
+        y1 = df[df['stop'] == 1]['laps']
+        plt.plot(x1, y1, label='first time')
+        # second stop
+        x2 = df[df['stop'] == 2]['proportion']
+        y2 = df[df['stop'] == 2]['laps']
+        plt.plot(x2, y2, label='second time')
+        if stop >= 3:
+            ax.set_ylim([0, 100])
+            x3 = df[df['stop'] == 3]['proportion']
+            y3 = df[df['stop'] == 3]['laps']
+            # plotting the line 3 points
+            plt.plot(x3, y3, label='third time')
+            if stop == 4:
+                ax.set_ylim([0, 100])
+                x4 = df[df['stop'] == 4]['proportion']
+                y4 = df[df['stop'] == 4]['laps']
+                # plotting the line 4 points
+                plt.plot(x4, y4, label='forth time')
+    ax.set(xlabel='Proportion of total laps',
+           ylabel='Race Count',
+           title=plot_title)
+    plt.legend()
+    return plt
+
+
+def df_group_plot(df: pd.DataFrame) -> pd.DataFrame:
+    """
+
+    :param df:
+    :return:
+    """
+    return df.groupby(by=['stop', 'proportion']).count().reset_index()
 
 
 def read_data(file):
@@ -64,13 +121,16 @@ def get_total_pitstop(pitstop_table):
       raceId driverId stop
     0      1        1    3
     """
-    pitstop_df = pd.DataFrame(columns=["raceId","driverId","stop"])
-    for line in range(0,len(pitstop_table)):
-        df_stop = pitstop_df.loc[(pitstop_df["raceId"] == int(pitstop_table.loc[line, "raceId"])) & (pitstop_df["driverId"] == int(pitstop_table.loc[line, "driverId"])), ["stop"]]
+    pitstop_df = pd.DataFrame(columns=["raceId", "driverId", "stop"])
+    for line in range(0, len(pitstop_table)):
+        df_stop = pitstop_df.loc[(pitstop_df["raceId"] == int(pitstop_table.loc[line, "raceId"])) & (
+                pitstop_df["driverId"] == int(pitstop_table.loc[line, "driverId"])), ["stop"]]
         if len(df_stop) > 0:
             stop_list = df_stop.values.tolist()
             if int(pitstop_table.loc[line, "stop"]) > stop_list[0][0]:
-                pitstop_df.loc[(pitstop_df["raceId"] == int(pitstop_table.loc[line, "raceId"])) & (pitstop_df["driverId"] == int(pitstop_table.loc[line, "driverId"])), "stop"] = int(pitstop_table.loc[line, "stop"])
+                pitstop_df.loc[(pitstop_df["raceId"] == int(pitstop_table.loc[line, "raceId"])) & (
+                        pitstop_df["driverId"] == int(pitstop_table.loc[line, "driverId"])), "stop"] = int(
+                    pitstop_table.loc[line, "stop"])
         else:
             pitstop_df = pitstop_df.append(
                 {"raceId": int(pitstop_table.loc[line, "raceId"]), "driverId": int(pitstop_table.loc[line, "driverId"]),
@@ -79,7 +139,6 @@ def get_total_pitstop(pitstop_table):
 
 
 def join_table(table_a, table_b, key_list):
-
     joined_table = table_a.merge(table_b, on=key_list)
     return joined_table
 
